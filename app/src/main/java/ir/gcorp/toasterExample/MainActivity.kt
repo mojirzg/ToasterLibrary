5975ca7f87497bcf12b0b1b65e79d6145778bab1
mojirzg <moji.razaghi@gmail.com>
2018-12-07 23:49:09 +0330

    open class

--- app/src/main/java/ir/gcorp/toasterExample/MainActivity.kt
+++ app/src/main/java/ir/gcorp/toasterExample/MainActivity.kt
@@ -2,11 +2,14 @@ package ir.gcorp.toasterExample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ir.gcorp.toasterlibrary.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ToasterMessage(this).show("sss")
     }
 }
--- toasterlibrary/src/main/java/ir/gcorp/toasterlibrary/ToasterMessage.kt
+++ toasterlibrary/src/main/java/ir/gcorp/toasterlibrary/ToasterMessage.kt
@@ -3,10 +3,11 @@ package ir.gcorp.toasterlibrary
 import android.content.Context
 import android.widget.Toast
 
-class ToasterMessage(val context : Context) {
+open class ToasterMessage(val context : Context) {
 
     fun show(text : String){
         Toast.makeText(context,text,Toast.LENGTH_SHORT).show()
     }
+
 
 }
